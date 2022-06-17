9bd86dfdc6255a2eeb7322cca0d081839e1b7577
scott Tresor <scotttresor@gmail.com>
2022-06-17 17:57:52 +0200

    initial commit theme with shopware

---
+++ src/SwagExerciceTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace SwagExerciceTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class SwagExerciceTheme extends Plugin implements ThemeInterface
+{
+}
